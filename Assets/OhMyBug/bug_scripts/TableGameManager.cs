4e0c5d0266c6a5707f842f01c98c391d9351e94e
OhMyBug <sa0963278610@gmail.com>
2021-06-04 21:11:46 +0800

    player done

---
+++ Assets/OhMyBug/bug_scripts/TableGameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TableGameManager : MonoBehaviour
+{
+    public Text time;
+    public Player player;
+    public Text Points;
+    public float TotalTime = 60;
+
+    private int CurrentPoints = 0;
+    private float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Points.gameObject.SetActive(true);
+        time.gameObject.SetActive(true);
+        t = TotalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.CurrentPoints = player.getPoints();
+        Points.text = "Points: " + CurrentPoints;
+        if( t > 0 )
+        {
+            t -= Time.deltaTime;
+            time.text = "" + t;
+        } 
+        else
+        {
+            Debug.Log("Time has run out!");
+            time.text = "Time's up !!";
+        }
+    }
+}
--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,10 +5,20 @@ using VRTK;
 
 public class Player : MonoBehaviour
 {
+    public GameObject Stair;
+    public GameObject hp;
+    public Material CorrectMaterial;
+    public Material WrongMaterial;
+
+    Rigidbody playerRigidbody;
+
+    private bool grounded = true;
+    private int point = 0;
     private int is_init = 0;
     // Start is called before the first frame update
     void Start()
     {
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,11 +34,53 @@ public class Player : MonoBehaviour
         for(int i = 0 ; i < Controllers.Length ; i++){
             var controller = Controllers[i];
             controller.TriggerPressed += SkipDialogue;
+            controller.GripPressed += GripHp;
+            controller.TouchpadPressed += Jump;
         }
         is_init = Controllers.Length;
     }
 
     void SkipDialogue(object o, ControllerInteractionEventArgs e){
         UIManager.Instance.Dialogue_Continue();
+    }
+
+    void GripHp(object o, ControllerInteractionEventArgs e)
+    {
+        hp.SetActive(false);
+        Stair.SetActive(true);
+    }
+
+    void Jump(object o, ControllerInteractionEventArgs e)
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        if (grounded == true)
+        {
+            playerRigidbody.velocity += new Vector3(0, 5, 0); //添加加速度
+            playerRigidbody.AddForce(Vector3.up * 20); //给刚体一个向上的力，力的大小为Vector3.up*mJumpSpeed
+            grounded = false;
+        }
+        Debug.Log("now points: " + this.point);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        grounded = true;
+        Renderer rend = collision.gameObject.GetComponent<Renderer>();
+        if( rend )
+        {
+            if( rend.sharedMaterial.name == CorrectMaterial.name )
+            {
+                this.point += 50;
+            }
+            else if( rend.sharedMaterial.name == WrongMaterial.name )
+            {
+                this.point -= 100;
+            }
+        }
+        Debug.Log("now points: " + this.point);
+    }
+
+    public int getPoints()
+    {
+        return this.point;
     }
 }
