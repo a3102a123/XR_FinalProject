ee86d31f689315a14ceb419bf3a1f774d0b50343
yide <derek0989518150@gmail.com>
2021-06-04 20:54:25 +0800

    bed update

---
+++ Assets/Kador/light/script/main.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    HighlightsFX HighlightsFX;
+    Transform cam;
+    // Use this for initialization
+    void Start()
+    {
+        HighlightsFX = GameObject.Find("Main Camera").GetComponent<HighlightsFX>();
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        raycastObj();
+    }
+
+    void raycastObj()
+    {
+        updateSelected();
+    }
+    void updateSelected()
+    {
+        HighlightsFX.ClearOutlineData();
+        Renderer renderer = this.GetComponent<Renderer>();
+        List<Renderer> one = new List<Renderer>();
+        one.Add(renderer);
+        HighlightsFX.AddRenderers(one);
+    }
+
+    void roateThis(GameObject n)
+    {
+        n.transform.localEulerAngles += Vector3.up * Time.deltaTime * 100f;
+    }
+}
