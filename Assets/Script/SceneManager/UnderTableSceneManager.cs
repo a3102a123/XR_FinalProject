ed908418ed43e6385162fcbe66a5e8158b344ca6
DavidTu <a3102a123@gamil.com>
2021-06-05 22:19:20 +0800

    Finish basic UnderTable

--- Assets/Script/SceneManager/UnderTableSceneManager.cs
+++ Assets/Script/SceneManager/UnderTableSceneManager.cs
@@ -10,16 +10,22 @@ public class UnderTableSceneManager : MonoBehaviour
 
     [HeaderAttribute("行為選擇")]
     [SerializeField]
-    private EventDecider ActionEvent;
+    private SelectEvent ActionEvent;
+
+    private bool is_init = false;
     
     void Start()
     {
+        is_init = false;
         Start_dia.Activate();
     }
 
     void Update()
     {
-        
+        if( !is_init && UIManager.Instance.displayer == null){
+            ActionEvent.Enable();
+            is_init = true;
+        }
     }
 
     void Action(){
